8eb2a27bd5ca79c8e5a3a21c51dcec3eb31e61e8
Emmanuel <ainsgreyrat@gmail.com>
2024-01-16 10:59:36 -0500

    fix saveImage

--- src/utils/saveImage.ts
+++ src/utils/saveImage.ts
@@ -5,24 +5,32 @@ import { join } from 'path';
 export const saveImage = (imageUrl: string) => {
   const splittedPath = imageUrl.split("/");
   const imageName = splittedPath[splittedPath.length - 1]
-  const dest = join(process.cwd(), 'public', 'image' ,imageName)
+  const dest = join(process.cwd(), 'static', imageName)
 
   const file = fs.createWriteStream(dest);
+  file.on('error', function (err) {
+    console.log(err);
+  })
 
-  https
-    .get(imageUrl, function (response) {
-      response.pipe(file);
-      file.on('finish', function () {
-        file.close((err) => {
-          if (err) console.log(err);
+  
+  file.on('ready', function () {
+    https
+      .get(imageUrl, function (response) {
+        response.pipe(file);
+
+        file.on('finish', function () {
+          file.close((err) => {
+            if (err) console.log(err);
+          });
         });
-      });
-      file.on('error', function (err) {
-        console.log(err);
-      })
-    })
-    .on('error', function (err) {
-      fs.unlink(dest, () => { console.log(err) });
-    });
-};
 
+        file.on('error', function (err) {
+          console.log("error here")
+          console.log(err);
+        })
+      })
+      .on('error', function (err) {
+        fs.unlink(dest, () => { console.log("error here too!"), console.log(err) });
+      });
+  })
+};
