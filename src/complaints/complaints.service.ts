119e120891c5021451eb7ac9ebc7acc3bb9f0f50
Emmanuel <ainsgreyrat@gmail.com>
2024-02-09 18:14:10 -0500

    env fix

--- src/complaints/complaints.service.ts
+++ src/complaints/complaints.service.ts
@@ -131,7 +131,7 @@ export class ComplaintService {
         <h2 style="line-height: 1.5; text-align: center;">Gracias por la espera ${complaint.names}</h2>
         <p style="text-align: center;">Su caso registrado con c&oacute;digo ${complaint.code} ha sido atendido.</p>
         <p style="text-align: center;">Puedes revisar el resultado usando en el siguiente enlace:</p>
-        <p style="text-align: center;"> <a href="${process.env.FRONTEND_DOMAIN}/consulta/${complaint.code}" target="_blank" rel="noopener" aria-invalid="true">REVISAR ESTADO DE MI RECLAMO</a></p>
+        <p style="text-align: center;"> <a href="${process.env.VITE_REHABILITADORA_URI}/consulta/${complaint.code}" target="_blank" rel="noopener" aria-invalid="true">REVISAR ESTADO DE MI RECLAMO</a></p>
         </div>`,
       }
       
